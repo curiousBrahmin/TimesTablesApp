9e494c704abced142d2c1ee45732c690c6923ff9
curiousBrahmin <Rambo@Rambos-iMac.hsd1.tn.comcast.net>
2016-09-17 11:30:52 -0500

    TimesTableApp

--- TimeTablesApp/ViewController.swift
+++ TimeTablesApp/ViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func sliderPressed(_ sender: AnyObject) {
+        
+        table.reloadData()
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return 50
+        
+    }
 
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String((indexPath.row + 1) * Int((slider.value)*20))
+        
+        return cell
+        
+    }
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
